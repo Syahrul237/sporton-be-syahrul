98453259ed77731ce8e409ea405ece0d1601b306
Syahrul257 <syaahrulsaid257@gmail.com>
2026-04-15 00:17:10 +0700

    all updates

--- src/middlewares/upload.middleware.ts
+++ src/middlewares/upload.middleware.ts
@@ -2,31 +2,35 @@ import multer from "multer";
 import path from "path";
 import fs from "fs";
 
-// Ensure the uploads directory exists
 const uploadDir = "uploads";
 if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir)
+  fs.mkdirSync(uploadDir);
 }
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, uploadDir);
-    },
-    filename:(req, file, cb) =>{
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, uniqueSuffix + path.extname(file.originalname))
-    },
-})
+  destination: (req, file, cb) => {
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    const uniqeSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqeSuffix + path.extname(file.originalname));
+  },
+});
 
-const fileFilter = (req: any, file: Express.Multer.File, cb: multer.FileFilterCallback) => {
-    if(file.mimetype.startsWith("image/")) {
-        cb(null, true);
-    } else {
-        cb(new Error("Only image files are allowed!"));
-    }
+const fileFilter = (
+  req: any,
+  file: Express.Multer.File,
+  cb: multer.FileFilterCallback,
+) => {
+  if (file.mimetype.startsWith("image/")) {
+    cb(null, true);
+  } else {
+    cb(new Error("Hanya gambar yang boleh"));
+  }
 };
 
-export const upload = multer({ storage:storage,
-     fileFilter:fileFilter,
-     limits:{ fileSize: 5 * 1024 * 1024 } // 5 MB limit
+export const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+  limits: { fileSize: 5 * 1024 * 1024 }, // maksimal 5 MB
 });
